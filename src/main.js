c4d467b1b51aa753eb8f274d392da04e45cae5bb
Lithium Sea <a.muell@gmail.com>
2026-04-11 12:52:02 +0200

    Replace ping-pong playback with 0.4x slow-motion loop

--- src/main.js
+++ src/main.js
@@ -1,20 +1,4 @@
 import './style.css'
 
 const video = document.querySelector('.hero-video-wrap video')
-video.removeAttribute('loop')
-let forward = true
-
-video.addEventListener('ended', () => {
-  forward = false
-  video.play()
-})
-
-video.addEventListener('timeupdate', () => {
-  if (!forward) {
-    video.currentTime -= 0.05
-    if (video.currentTime <= 0) {
-      forward = true
-      video.play()
-    }
-  }
-})
+video.playbackRate = 0.4
