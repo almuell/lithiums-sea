b0d85ea1961c13c4c15e1ace2a73191015674cdf
Lithium Sea <a.muell@gmail.com>
2026-04-11 12:50:13 +0200

    Ping-pong hero video playback (forward then reverse)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import './style.css'
+
+const video = document.querySelector('.hero-video-wrap video')
+video.removeAttribute('loop')
+let forward = true
+
+video.addEventListener('ended', () => {
+  forward = false
+  video.play()
+})
+
+video.addEventListener('timeupdate', () => {
+  if (!forward) {
+    video.currentTime -= 0.05
+    if (video.currentTime <= 0) {
+      forward = true
+      video.play()
+    }
+  }
+})
